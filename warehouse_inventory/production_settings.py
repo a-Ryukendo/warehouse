319bbedd2aacceca62276aee25865bc7865bb604
a-Ryukendo <130548884+a-Ryukendo@users.noreply.github.com>
2025-07-27 15:54:00 +0530

    Add files via upload

---
+++ warehouse_inventory/production_settings.py
@@ -0,0 +1,54 @@
+"""
+Production settings for warehouse_inventory project.
+Configured for PythonAnywhere deployment.
+"""
+
+from .settings import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# Update this with your PythonAnywhere domain
+ALLOWED_HOSTS = ['YOUR_USERNAME.pythonanywhere.com', 'www.YOUR_USERNAME.pythonanywhere.com']
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/YOUR_USERNAME/YOUR_REPO_NAME/staticfiles'
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/home/YOUR_USERNAME/YOUR_REPO_NAME/media'
+
+# Security settings
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+
+# Database (using SQLite for PythonAnywhere free tier)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Logging configuration
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': '/home/YOUR_USERNAME/YOUR_REPO_NAME/django.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+} 
